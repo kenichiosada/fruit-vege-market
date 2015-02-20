a611c71b1f9246caf69fa753b10585c13dc4d249
fruitvegemarket <fruitvegemarket@outlook.com>
2015-02-20 08:08:26 +0000

    wip

--- openshift/lib/DB/Schema/ResultSet/Trade.pm
+++ openshift/lib/DB/Schema/ResultSet/Trade.pm
@@ -15,6 +15,8 @@ has 'crop_id' => (
 
 sub select_by_crop_id {
   my $self    = shift;
+
+  my $interval = "> now() - interval '30 days'";
  
   my @rs = $self->search(
     { 
@@ -23,13 +25,13 @@ sub select_by_crop_id {
     { 
       join     => 'trade_details',
       prefetch => 'trade_details',
+      trade_date => \$interval,
       order_by => [ 
                     { -desc => 'trade_date' },
                     { -asc  => 'trade_method' },
                     { -asc  => 'breed' },
                     { -desc => 'market' },
                   ],
-      rows     => 30,
 			result_class => 'DBIx::Class::ResultClass::HashRefInflator',
     }
   );
